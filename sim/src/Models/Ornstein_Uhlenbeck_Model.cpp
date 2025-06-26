688764a3da4c8ab99dd743ea78bd56a38d50b5e7
alberto <alvillalbac@gmail.com>
2025-06-26 23:29:55 +0200

    tests setup

--- sim/src/Models/Ornstein_Uhlenbeck_Model.cpp
+++ sim/src/Models/Ornstein_Uhlenbeck_Model.cpp
@@ -5,15 +5,15 @@ OrnsteinUhlenbeckModel::OrnsteinUhlenbeckModel(const ModelData& modelData, std::
     meanReversionSpeed(modelData.reversionSpeed        )
 {}
 
+ModelData OrnsteinUhlenbeckModel::getModelData() const {
+
+    return ModelData(drift, volatility, initialPrice, meanReversionSpeed);
+}
+
 double OrnsteinUhlenbeckModel::mu(double price_t, double time){
 
     (void) time;
     return meanReversionSpeed * (drift - price_t);
-}
-
-ModelData OrnsteinUhlenbeckModel::getModelData() const {
-
-    return ModelData(drift, volatility, initialPrice, meanReversionSpeed);
 }
 
 double OrnsteinUhlenbeckModel::sigma(double price_t, double time){
